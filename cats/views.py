a4ab9d003c8d14f899515db90ddb2355a195befb
Egor Korotaev <ego_kor@mail.ru>
2024-12-21 12:20:10 +0300

    After Sprint 14/19 Topic 1/3 Lesson 3/5

--- cats/views.py
+++ cats/views.py
@@ -1,14 +1,18 @@
 from rest_framework import permissions, viewsets
+from rest_framework.throttling import ScopedRateThrottle
 
 from .models import Achievement, Cat, User
 from .permissions import OwnerOrReadOnly, ReadOnly
 from .serializers import AchievementSerializer, CatSerializer, UserSerializer
+from .throttling import WorkingHoursRateThrottle
 
 
 class CatViewSet(viewsets.ModelViewSet):
     queryset = Cat.objects.all()
     serializer_class = CatSerializer
     permission_classes = (OwnerOrReadOnly,)
+    throttle_classes = (WorkingHoursRateThrottle, ScopedRateThrottle)
+    throttle_scope = 'low_request'
 
     def get_permissions(self):
         if self.action == 'retrieve':
@@ -16,7 +20,7 @@ class CatViewSet(viewsets.ModelViewSet):
         return super().get_permissions()
 
     def perform_create(self, serializer):
-        serializer.save(owner=self.request.user) 
+        serializer.save(owner=self.request.user)
 
 
 class UserViewSet(viewsets.ReadOnlyModelViewSet):
